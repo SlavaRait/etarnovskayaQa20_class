e6814a6faa9a91cdd8ddcf9c10ffff4cda7f3dbb
eenat <etarnovskaya@hotmail.com>
2019-07-05 12:03:24 +0300

    extract methods and parameters

--- trello-web/src/test/java/TestBase.java
+++ trello-web/src/test/java/TestBase.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -19,5 +20,57 @@ public class TestBase {
   @AfterMethod
   public void tearDown() {
     wd.quit();
+  }
+
+  public void pause(int millis) throws InterruptedException {
+    Thread.sleep(millis);
+  }
+
+  public void confirmLogin() {
+    click(By.id("login"));
+  }
+
+  public void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  public void fillLoginForm(String email, String password) {
+    type(By.id("user"), email);
+    type(By.name("password"), password);
+  }
+
+  public void type(By locator, String text) {
+    click(locator);
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
+  }
+
+  public void clickLoginButton() {
+    click(By.cssSelector("[href='/login']"));
+  }
+
+  public void login(String email, String pwd) throws InterruptedException {
+    clickLoginButton();
+    fillLoginForm(email, pwd); //"elena.telran@yahoo.com", "12345.com"
+    pause(3000);
+    confirmLogin();
+    pause(10000);
+  }
+
+  public  void confirmBoardCreation(){
+    click(By.cssSelector("[data-test-id=header-create-board-submit-button]"));
+  }
+
+  public void typeBoardName(String boardName) {
+    type(By.cssSelector("[data-test-id=header-create-board-title-input]"), boardName);
+
+  }
+
+  public void selectCreateBoardFromDropDown() {
+    click(By.cssSelector("[data-test-id=header-create-board-button]"));
+  }
+
+  public void clickOnPlusButtonOnHeader() {
+    click(By.cssSelector("[name=add]"));
   }
 }
--- trello-web/src/test/java/TrelloLoginTest.java
+++ trello-web/src/test/java/TrelloLoginTest.java
@@ -1,28 +1,16 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class TrelloLoginTest  extends  TestBase{
 
   @Test
   public void loginTest() throws InterruptedException {
-    //click login button
-    wd.findElement(By.cssSelector("[href='/login']")).click();
-    //type email
-    wd.findElement(By.id("user")).sendKeys("elena.telran@yahoo.com");
-    //type password
-    wd.findElement(By.name("password")).sendKeys("12345.com");
-    Thread.sleep(3000);
-    //confirmLogin
-    wd.findElement(By.id("login")).click();
-    Thread.sleep(10000);
+    clickLoginButton();
+    fillLoginForm("elena.telran@yahoo.com", "12345.com");
+    pause(3000);
+    confirmLogin();
+    pause(10000);
 
   }
-
 
 }
