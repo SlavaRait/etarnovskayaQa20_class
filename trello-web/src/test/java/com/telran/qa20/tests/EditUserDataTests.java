929ae4e4c7e95d838795ea20b23afac95573a20e
eenat <etarnovskaya@hotmail.com>
2019-07-30 20:39:39 +0300

    logger moved to TestBase

--- trello-web/src/test/java/com/telran/qa20/tests/EditUserDataTests.java
+++ trello-web/src/test/java/com/telran/qa20/tests/EditUserDataTests.java
@@ -1,16 +1,19 @@
 package com.telran.qa20.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class EditUserDataTests extends  TestBase {
 
-  
+
   @Test
   public void addAvatarTest() throws InterruptedException {
     app.getUser().clickOnAvatar();
     app.getUser().enterToProfile();
     app.getUser().changeAvatar();
-    //app.getUser().saveChanges();
+
+
 
   }
 }
--- trello-web/src/test/java/com/telran/qa20/tests/TestBase.java
+++ trello-web/src/test/java/com/telran/qa20/tests/TestBase.java
@@ -2,25 +2,42 @@ package com.telran.qa20.tests;
 
 import com.telran.qa20.manager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class TestBase {
+import java.lang.reflect.Method;
 
+public class TestBase {
    static  ApplicationManager app =
            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  Logger logger =
+          LoggerFactory.getLogger(TestBase.class);
 
   @BeforeSuite
   public void setUp() throws InterruptedException {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
+
+  @BeforeMethod
+  public void startLogger(Method m){
+    logger.info("Start test  " + m.getName());
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void stopLogger(Method m){
+    logger.info("Stop test  " + m.getName());
+  }
+
 
 
 }
