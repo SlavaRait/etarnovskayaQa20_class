d00de4bc58fc874b7c37dfc00ca144ad3e05c720
eenat <etarnovskaya@hotmail.com>
2019-07-30 19:59:35 +0300

    added avatar

--- trello-web/src/test/java/com/telran/qa20/tests/EditUserDataTests.java
+++ trello-web/src/test/java/com/telran/qa20/tests/EditUserDataTests.java
@@ -3,12 +3,14 @@ package com.telran.qa20.tests;
 import org.testng.annotations.Test;
 
 public class EditUserDataTests extends  TestBase {
+
+  
   @Test
-  public void addAvatarTest(){
+  public void addAvatarTest() throws InterruptedException {
     app.getUser().clickOnAvatar();
     app.getUser().enterToProfile();
     app.getUser().changeAvatar();
-    app.getUser().saveChanges();
+    //app.getUser().saveChanges();
 
   }
 }
